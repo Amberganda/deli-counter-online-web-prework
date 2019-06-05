28f67e8602154635369a96d13974352346a961a9
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 04:11:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line(array)
     if array.size == 0
         puts "The line is currently empty."
-        
+
     else
         line_position = 1
-        array.each do |name|        
+        array.each do |name|
         puts "The line is currently: #{line_position}. #{name} "
          
 end
