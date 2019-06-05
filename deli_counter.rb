acceb4e00712b485de6cba32bac0b5afd35e81f9
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 04:11:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line(array)
     if array.size == 0
         puts "The line is currently empty."
-    end
+        
+    else
+        line_position = 1
+        array.each do |name|        
+        puts "The line is currently: #{line_position}. #{name} "
+         
 end
