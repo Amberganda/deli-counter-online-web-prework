6b16ea4aaedb5707b765db5e27052b5a43d6f108
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 04:40:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,9 @@ def take_a_number(array, name)
     array.push(name)
     puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+    if array.size == 0
+        puts ""
+    end
+end
